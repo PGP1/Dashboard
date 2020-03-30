c7b0719f871ac40d532538f95ff2001ecbd93a5d
Austin <rockmanvnx6@gmail.com>
2020-03-30 15:43:42 +1100

    link test

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Index() {
     return (
         <div>
             <p>Hello</p>
+            <Link href={"/about"}>
+                <a> About </a>
+            </Link>
         </div>
     );
 }
