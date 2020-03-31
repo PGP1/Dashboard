4a526e83193cf21e3da2b6d9378f86981653baf4
Austin <rockmanvnx6@gmail.com>
2020-03-31 13:14:12 +1100

    validation

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
+    webpackDevMiddleware(config) {
+        config.watchOptions = {
+            ignored: [
+                /\.git\//,
+                /\.next\//,
+                /node_modules/
+            ]
+        }
+        return config;
+    },
     distDir: 'build',
     exportPathMap: () => ({
         '/': { page: '/' },
-      }),
+        '/register': { page: '/register' }
+    }),
     assetPrefix: isProduction ? '/Dashboard' : '',
     publicRuntimeConfig: {
         // used in '/components/Link.js/', for more details go to the component itself
--- pages/register.js
+++ pages/register.js
@@ -1,27 +1,38 @@
 import Layout from "../components/Layout.js";
 import style from "./styles/register.module.scss";
-import { Button, Checkbox, Form } from 'semantic-ui-react';
+import { Button, Checkbox, Form, Message } from 'semantic-ui-react';
 import Animation from "../components/Animation";
 import Particles from 'react-particles-js';
 import React, { Component } from 'react';
 import AWSController from "../api/AWSController"
+import SimpleReactValidator from 'simple-react-validator';
 
 class Register extends Component {
 
     constructor(props) {
-        super(props)
+        super(props);
+        this.validator = new SimpleReactValidator();
+        this.state = {};
     }
 
     handleChange = () => {
-        this.setState({ [event.target.name]: event.target.value });
-    }
+        this.setState({ [event.target.name]: event.target.value, error: !this.validator.allValid() });
+    };
 
     handleRegister = () => {
-        const { username, password, email } = this.state;
-        AWSController.signUp(username, password, email);
-    }
+        if (this.validator.allValid()) {
+            this.setState({ error: false });
+            const { username, password, email } = this.state;
+            AWSController.signUp(username, password, email);
+        } else {
+            this.validator.showMessages();
+            this.setState({ error: true });
+            this.forceUpdate();
+        }
+    };
 
     render() {
+        console.log('this validate', this.validator.allValid(), this.validator);
         return (
             <Layout>
                 <div className={style.container}>
@@ -47,26 +58,34 @@ class Register extends Component {
                             <h2 className="header">
                                 Register
                             </h2>
-                            <Form>
+                            <Form error={!this.validator.allValid()}>
                                 <Form.Field>
                                     <label>Email</label>
-                                    <input placeholder='Email' name="email" 
+                                    <input placeholder='Email' name="email" value={this.state.email}
                                                 onChange={this.handleChange}/>
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Username</label>
-                                    <input placeholder='Username' name="username" 
+                                    <input placeholder='Username' name="username" value={this.state.username}
                                                 onChange={this.handleChange}/>
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Password</label>
-                                    <input placeholder='Password' type="password" name="password"
+                                    <input placeholder='Password' type="password" name="password" value={this.state.password}
                                                 onChange={this.handleChange}/>
                                 </Form.Field>
                                 <Form.Field>
                                     <Checkbox label='I agree to the Terms and Conditions' />
                                 </Form.Field>
-                                <Button type='submit' onClick={this.handleRegister}>Register</Button>
+                                <Message error>
+                                    <Message.Header>Uh oh!</Message.Header>
+                                    <Message.List>
+                                        <Message.Item>{this.validator.message('Email', this.state.email, 'required|email')}</Message.Item>
+                                        <Message.Item>{this.validator.message('Username', this.state.username, 'required|alpha_num')}</Message.Item>
+                                        <Message.Item>{this.validator.message('Password', this.state.password, 'required')}</Message.Item>
+                                    </Message.List>
+                                </Message>
+                                <Button primary={this.validator.allValid()} type='submit' onClick={this.handleRegister}>Next</Button>
                             </Form>
                         </div>
                     </div>
