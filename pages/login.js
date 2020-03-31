96498d105eaefa0a212d4dbf4e6cc790992ff645
David Vo <thedavidvo@gmail.com>
2020-03-31 19:57:11 +1100

    Austin crash course

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Test from "../components/Test";
+
+class Login extends Component {
+    render() {
+        return (
+            <>
+                <p> Hi </p>
+                <Test/>
+            </>
+        )
+    }
+}
+
+export default Login;
