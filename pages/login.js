57179f240fff5c45a188f67e9646dd564d16d8b8
David Vo <thedavidvo@gmail.com>
2020-04-02 17:53:23 +1100

    Fixing of some bugs, tweaks to UX

--- pages/login.js
+++ pages/login.js
@@ -5,43 +5,41 @@ import Animation from '../components/Animation';
 import Particles from 'react-particles-js';
 import Layout from '../components/Layout'
 class Login extends Component {
-  
-  render() {
-      const particles_config = require('./assets/config/particles_config.json');
+
+    render() {
+        const particles_config = require('./assets/config/particles_config.json');
         return (
             <Layout>
-            <div className={style.container}>
-                <div className={style.left}>
-                    <Particles className={style.left} params={particles_config} />
-                </div>
+                <div className={style.container}>
+                    <div className={style.left}>
+                        <Particles className={"background"} params={particles_config} />
+                    </div>
 
-                <div className={style.right}>
-                    <div className={style.form}>
-                        <div className={style.logo}>
-                            <Animation />
-                        </div>
-                        <h2 className="header">Login</h2>
-                        <div className={style.content}>
-                            <Form>
-                                <Form.Field>
-                                    <label>Username</label>
-                                    <input placeholder='Username' name="username" />
-                                </Form.Field>
-                                <Form.Field>
-                                    <label>Password</label>
-                                    <input placeholder='Password' type='password' name="password" />
-                                </Form.Field>
-                                <div className={style.button}>
-                                    <Button animated>
-                                        <Button.Content visible>Login</Button.Content>
-                                        <Button.Content hidden><Icon name='arrow right' /></Button.Content>
-                                    </Button>
-                                </div>
-                            </Form>
+                    <div className={style.right}>
+                        <Animation/>
+                        <div className={style.form}>
+                            <h2 className="header">Login</h2>
+                            <div className={style.content}>
+                                <Form>
+                                    <Form.Field>
+                                        <label>Username</label>
+                                        <input placeholder='Username' name="username" />
+                                    </Form.Field>
+                                    <Form.Field>
+                                        <label>Password</label>
+                                        <input placeholder='Password' type='password' name="password" />
+                                    </Form.Field>
+                                    <div className={style.button}>
+                                        <Button animated>
+                                            <Button.Content visible>Login</Button.Content>
+                                            <Button.Content hidden><Icon name='arrow right' /></Button.Content>
+                                        </Button>
+                                    </div>
+                                </Form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </Layout>
         )
     }
