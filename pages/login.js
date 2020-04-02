92f871b08b899418887296786d9d11a14e897955
David Vo <thedavidvo@gmail.com>
2020-04-02 19:12:12 +1100

    responsiveness fix

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import style from './styles/login.module.scss';
 import { Button, Form, Icon } from 'semantic-ui-react';
+import Link from 'next/link';
 import Animation from '../components/Animation';
 import Particles from 'react-particles-js';
 import Layout from '../components/Layout'
@@ -13,10 +14,16 @@ class Login extends Component {
                 <div className={style.container}>
                     <div className={style.left}>
                         <Particles className={"background"} params={particles_config} />
+
+
+                        <div className={style.description}>
+                            <h1>Log in to track your plants!</h1>
+                            <h3>Sunt nulla anim consectetur aute ea officia fugiat velit consectetur reprehenderit.</h3>
+                        </div>
                     </div>
 
                     <div className={style.right}>
-                        <Animation/>
+                        <Animation />
                         <div className={style.form}>
                             <h2 className="header">Login</h2>
                             <div className={style.content}>
@@ -29,11 +36,24 @@ class Login extends Component {
                                         <label>Password</label>
                                         <input placeholder='Password' type='password' name="password" />
                                     </Form.Field>
-                                    <div className={style.button}>
+                                    {/* {error && <Message error>
+                                        <Message.Header>Uh oh!</Message.Header>
+                                        <Message.List>
+                                            {this.state.serverError}
+                                        </Message.List>
+                                    </Message>} */}
+                                    <div className="space-between flex align-center">
                                         <Button animated>
                                             <Button.Content visible>Login</Button.Content>
                                             <Button.Content hidden><Icon name='arrow right' /></Button.Content>
                                         </Button>
+                                        <Link href={"/register"}>
+                                            <a>
+                                                Don't have an account?
+                                            </a>
+                                        </Link>
+
+
                                     </div>
                                 </Form>
                             </div>
