62ca9dd93740061d9414806a2c3a1f16725370c6
Austin <rockmanvnx6@gmail.com>
2020-04-10 14:11:50 +1000

    api log in

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,16 @@
 import style from './styles/Nav.module.scss';
+import AWSController from '../api/AWSController';
+import { useState } from 'react';
 
 export default function Nav (props) {
+    const [username, setUsername] = useState();
+    AWSController.getCurrentUserName().then(username => setUsername(username));
     return (
         <div className={style.nav}>
             <div className={style.logo}>Logo</div>
-            {!props?.loggedIn && <div className={style.accountHolder}>
-                <div className={style.avatar}></div>
-                <div className={style.username}>username</div>
+            {props?.isAuthenticated && <div className={style.accountHolder}>
+                <div className={style.avatar}/>
+                <div className={style.username}>{username}</div>
             </div>}
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,83 @@
 import Link from "../components/Link";
+import React, { Component } from 'react';
+import style from "./styles/device.module.scss";
+import Layout from "../components/Layout";
+import AWSController from "../api/AWSController";
+import Router from "next/router";
+class Index extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAuthenticated: false,
+            user: null
+        }
+    }
 
-export default function Index() {
-    return (
-        <div>
-            <p>Hello</p>
-            <Link href={"/register"}>
-                <a> register </a>
-            </Link>
-            <br/>
-            <Link href={"/login"}>
-                <a> login </a>
-            </Link>
-            <br/>
-            <Link href={"/device"}>
-                <a> device </a>
-            </Link>
-        </div>
-    );
+    setAuthenticate = () => {
+        this.setState({ isAuthenticated: true });
+    };
+
+    setUser = (user) => {
+        this.setState({ user })
+    };
+
+    componentDidMount() {
+        AWSController.getCurrentSession().then(session => {
+            this.setAuthenticate(true);
+            console.log("session", session);
+            AWSController.getCurrentSession().then(user => {
+                this.setUser(user);
+                console.log("user", this.state.user);
+            });
+        }).catch(err => Router.push("/login"));
+
+        this.setState({ isAuthenticating: false });
+    }
+
+    render() {
+        const {isAuthenticated} = this.state;
+        return (
+            <>
+                {isAuthenticated &&
+                <Layout isAuthenticated={isAuthenticated}>
+                    <div className={style.container}>
+                        <div className={style.formCenter}>
+                            <div className={style.form}>
+                                <div className={style.content}>
+                                    <h1> Register your devices </h1>
+                                    <table className="ui blue table center aligned segment">
+                                        <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>Device 1</td>
+                                            <td>Online</td>
+                                            <td>Access / Shutdown</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Device 1</td>
+                                            <td>Online</td>
+                                            <td>Access / Shutdown</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+
+                                    <div className={style.bottom}>
+                                        <button className=" ui right floated primary button"> Add device</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Layout>
+                }
+            </>)
+    }
 }
+
+export default Index;
