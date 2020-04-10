e19b308cecc97706950913c3b88cceecc5e8e656
David Vo <thedavidvo@gmail.com>
2020-04-10 15:45:19 +1000

    added modal to adding a device

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,25 @@ import style from "./styles/device.module.scss";
 import Layout from "../components/Layout";
 import AWSController from "../api/AWSController";
 import Router from "next/router";
+import { Modal, Form, Icon, Button } from 'semantic-ui-react';
+
 class Index extends Component {
     constructor(props) {
         super(props);
         this.state = {
             isAuthenticated: false,
-            user: null
+            user: null,
+            open: false
         }
     }
+
+    show = (size, dimmer) => () => {
+        this.setState({ size, dimmer, open: true })
+    };
+
+    close = () => {
+        this.setState({ open: false })
+    };
 
     setAuthenticate = () => {
         this.setState({ isAuthenticated: true });
@@ -35,46 +46,61 @@ class Index extends Component {
     }
 
     render() {
-        const {isAuthenticated} = this.state;
+        const { isAuthenticated, open, size, dimmer } = this.state;
+
         return (
             <>
                 {isAuthenticated &&
-                <Layout isAuthenticated={isAuthenticated}>
-                    <div className={style.container}>
-                        <div className={style.formCenter}>
-                            <div className={style.form}>
-                                <div className={style.content}>
-                                    <h1> Register your devices </h1>
-                                    <table className="ui blue table center aligned segment">
-                                        <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Status</th>
-                                            <th>Action</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <td>Device 1</td>
-                                            <td>Online</td>
-                                            <td>Access / Shutdown</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Device 1</td>
-                                            <td>Online</td>
-                                            <td>Access / Shutdown</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
+                    <Layout isAuthenticated={isAuthenticated}>
+                        <div className={style.container}>
+                            <div className={style.formCenter}>
+                                <div className={style.form}>
+                                    <div className={style.content}>
+                                        <h1> Register your devices </h1>
+                                        <table className="ui blue table center aligned segment">
+                                            <thead>
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th>Status</th>
+                                                    <th>Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>Device 1</td>
+                                                    <td>Online</td>
+                                                    <td>Access / Shutdown</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Device 1</td>
+                                                    <td>Online</td>
+                                                    <td>Access / Shutdown</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
 
-                                    <div className={style.bottom}>
-                                        <button className=" ui right floated primary button"> Add device</button>
+                                        <div className={style.bottom}>
+                                            <button onClick={this.show('small', 'blurring')} className="ui right floated primary button"> Add device</button>
+                                            <Modal dimmer={dimmer} size={size} open={open} onClose={this.close}>
+                                                <Modal.Header>Add a device</Modal.Header>
+                                                <Modal.Content>
+                                                    <Form>
+                                                        <Form.Field required>
+                                                            <label>Device Name</label>
+                                                            <input />
+                                                        </Form.Field>
+                                                    </Form>
+                                                </Modal.Content>
+                                                <Modal.Actions>
+                                                    <Button content='Add' primary />
+                                                </Modal.Actions>
+                                            </Modal>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </Layout>
+                    </Layout>
                 }
             </>)
     }
