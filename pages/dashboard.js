6d7af8a1faf2e2ad433fb69ea7b1c8e400064cb1
David Vo <thedavidvo@gmail.com>
2020-04-24 21:44:12 +1000

    chart.js added into view

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,46 +2,166 @@ import React, { Component } from 'react';
 import Sidenav from '../Components/Sidenav';
 import Layout from '../Components/Layout';
 import style from './styles/dashboard.module.scss';
+import { Doughnut, Bar, HorizontalBar, Line, Radar } from 'react-chartjs-2';
+//import pi_data from './assets/config/pi-data.json';
+// import LineGraph from '../components/LineGraph';
+// import MixedChart from '../components/MixedChart';
 
 
 class Dashboard extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: {
+                datasets: [{
+                    data: [10.3, 4.8, 550, 60, 28]
+                }],
+                labels: [
+                    'Water',
+                    'pH',
+                    'LDR',
+                    'Humidity',
+                    'Temperature'
+                ]
+            }
+        }
+    }
+
     render() {
         return (
             <Layout className={style.container}>
 
                 <Sidenav />
-                
+
                 <div className={style.dashboardContent}>
 
-                   <div className={style.dashboardGridContent}> 
-                    
-                    <div className={style.dashboardDeviceStatus}>
-                        <div className={style.dashboardDeviceStatusContainer}>
-                            <div className={style.dashboardDeviceStatusHeader}>
-                                Device Status
+                    <div className={style.purpleBackground} />
+
+
+                    <div className={style.dashboardGridContent}>
+
+                        <div className={style.left}>
+                            <div className={[style.box, style.dashboardDeviceStatus].join(" ")}>
+                                <div className={style.dashboardHeader}>
+                                    Device Status
+                                </div>
+                                <div className={style.dashboardDeviceStatusGrid}>
+                                    <div>Device ID</div>
+                                    <div style={{ color: "#BDBDBD" }}>3a0e3db35c8890c8</div>
+                                    <div>Uptime</div>
+                                    <div style={{ color: "#BDBDBD" }}>2h30m</div>
+                                </div>
+                                <div className={style.dashboardDeviceStatusButtons}>
+                                    <button className="ui yellow button">Sleep</button>
+                                    <button className="ui black button">Restart</button>
+                                </div>
                             </div>
 
-                            <div className={style.dashboardDeviceStatusGrid}>
-                                <div>Device ID</div>
-                                <div>3a0e3db35c8890c8</div>
-                                <div>Uptime</div>
-                                <div>2h30m</div>
+                            <div className={style.box}>
+                                <div className={style.dashboardHeader}>
+                                    Water Level
+                                </div>
+                                <div className={style.chart}>
+                                    <Line
+                                        data={this.state.data}
+                                        width={100}
+                                        height={300}
+                                        options={{
+                                            responsive: true,
+                                            maintainAspectRatio: false
+                                        }}
+                                    />
+                                </div>
                             </div>
 
-                            <div className={style.dashboardDeviceStatusButtons}>
-                                <button class="ui yellow button">Sleep</button>
-                                <button class="ui black button">Restart</button>
+                            <div className={style.box}>
+                                <div className={style.dashboardHeader}>
+                                    Temperature Level
+                                </div>
+                                <div className={style.chart}>
+                                    <Line
+                                        data={this.state.data}
+                                        width={100}
+                                        height={300}
+                                        options={{
+                                            responsive: true,
+                                            maintainAspectRatio: false
+                                        }}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className={style.box}>
+                                <div className={style.dashboardHeader}>
+                                    Humidity Level
+                                </div>
+                                <div className={style.chart}>
+                                    <Line
+                                        data={this.state.data}
+                                        width={100}
+                                        height={300}
+                                        options={{
+                                            responsive: true,
+                                            maintainAspectRatio: false
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={style.right}>
+
+
+                            <div className={[style.box, style.box2].join(" ")}>
+                                <div className={style.dashboardHeader}>
+                                    Overall details
+                                </div>
+                                <div className={style.chart}>
+                                    <HorizontalBar
+                                        data={this.state.data}
+                                        height={300}
+                                        options={{
+                                            responsive: true,
+                                            maintainAspectRatio: false
+                                        }} />
+                                </div>
+                            </div>
+
+                            <div className={style.box}>
+                                <div className={style.dashboardHeader}>
+                                    pH Level
+                                </div>
+                                <div className={style.chart}>
+                                    <Line
+                                        data={this.state.data}
+                                        width={100}
+                                        height={300}
+                                        options={{
+                                            responsive: true,
+                                            maintainAspectRatio: false
+                                        }}
+                                    />
+                                </div>
+                            </div>
+
+                            <div className={[style.box, style.box2].join(" ")}>
+                                <div className={style.dashboardHeader}>
+                                    Light Level
+                                </div>
+                                <div className={style.chart}>
+                                    <Line
+                                        data={this.state.data}
+                                        height={500}
+                                        options={{
+                                            responsive: true,
+                                            maintainAspectRatio: false
+                                        }} />
+                                </div>
                             </div>
                         </div>
                     </div>
-        
-                    </div>
-                </div>
-
-
-            </Layout>
-
-
+                </div >
+            </Layout >
         )
 
     }
